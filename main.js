54c60c186b612b69c6254e2eb9b0e6731b4ba5d5
Shaun O'Connor <shaun.oconnor@tequila.com.au>
2011-05-26 11:35:08 +1000

    Adding entire project

---
+++ main.js
@@ -0,0 +1,45 @@
+/**
+ * This does everything, i.e. load Delicious daily feed
+ */
+
+$(document).ready(function() {
+
+    var username = localStorage["delicousDailyUsername"];
+    var tag = localStorage["delicousDailyTag"];
+
+    if (!username || !tag || username == '' || tag == '') {
+        $("#output").html('<strong/><i><p>You need to set your delicious username and tag first!</p></i></strong>');
+    }
+
+    else {
+
+        $("#output").html('<strong/><i><p>Loading...</p></i></strong>');
+
+        var jqxhr = $.getJSON("http://feeds.delicious.com/v1/json/" + username + "/" + tag + "?callback=?",
+        function() {});
+
+        jqxhr.error(function() {
+            $("#output").html('<strong/><i><p>Error</p></i></strong>');
+        });
+
+        jqxhr.complete(function(data) {
+            $("#output").html('<strong/><i><p>Complete</p></i></strong>');
+        });
+
+        jqxhr.success(function(data) {
+
+            $.each(data,
+            function(i, item) {
+
+                chrome.tabs.create({
+                    url: item.u
+                },
+                function() {});
+
+            });
+
+            window.close();
+        });
+    }
+}
+);
---
+++ options.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+    console.log('options')
+
+    loadOptions();
+});
+
+function loadOptions() {
+
+    var delicousDailyUsername = localStorage["delicousDailyUsername"];
+    var delicousDailyTag = localStorage["delicousDailyTag"];
+
+    $("#current").html("Current username : " + delicousDailyUsername + "<br />" + "Current tag : " + delicousDailyTag);
+}
+
+function saveOptions() {
+
+    var usernameInput = document.getElementById("username").value;
+    var tagInput = document.getElementById("tag").value;
+
+    localStorage["delicousDailyUsername"] = usernameInput;
+    localStorage["delicousDailyTag"] = tagInput;
+
+    $("#current").html("Current username : " + usernameInput + "<br />" + "Current tag : " + tagInput);
+
+}
